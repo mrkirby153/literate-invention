3f1678bf2789a5b14cd7d6aebf5f434b55008793
mrkirby153 <mr.austinwhyte@gmail.com>
2017-04-05 18:15:16 +0000

    Add "natural language" processing
    The language processing consists of an action and a subject. The action will be performed on the subject.
    An example would be: "grab ball" or "throw spear" or "open door"

--- app.py
+++ app.py
@@ -1,7 +1,36 @@
 import classes
+import objects as Object
 
-class TestAction(classes.Action):
-  def __init__(self):
-    super(TestAction, self).__init__()
+loadedObjects = []
+
+
+def findObject(name):
+  for obj in loadedObjects:
+    if obj.name == name:
+      return obj
+  return None
+
+while True:
+  read = input("> ").lower()
+  
+  # Allow the user to quit the app gracefully
+  if read == "exit":
+    break
+  
+  split = read.split()
+  action = split[0]
+  obj = None
+  
+  if len(split) >= 2:
+    obj = split[1]
+  
+  # Find the object in the room
+  if obj != None:
+    obj = findObject(obj)
     
-TestAction()
+  # Perform the action on the object
+  try:
+    getattr(obj, action)()
+  except AttributeError:
+    print("Cannot perform that action on that object")
+    continue
